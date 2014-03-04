dad96a8b48cd8efb7604fc137272dd3af909eb90
Eddie Bijnen <ebijnen@desktop-31.students.os3.nl>
2014-03-04 17:48:28 +0100

    Added dedub on dmesg, added initial mysql statements

--- usb.py
+++ usb.py
@@ -3,21 +3,64 @@ import socket
 import platform
 import subprocess
 import sys
-
-dmesg = subprocess.Popen(['dmesg'], stdout = subprocess.PIPE).communicate()[0]
-
+import re
+import MySQLdb
 os = platform.system()
+usb_found = "False"
+
+
+
+
 
 if os == 'Linux':
-	print "linux check"
-	for line in dmesg.splitlines():
-    		if "SerialNumber:" in line:
-       			 print line
 
-else:
+	dmesg = subprocess.Popen(["/tmp/usb.sh"], stdout = subprocess.PIPE).communicate()[0]
+	for line in dmesg.splitlines():
+
+		if "> [" in line:
+
+    			if "SerialNumber:" in line:
+				 serial = re.findall(r'SerialNumber:.*',line)
+				 usb_found = "True"
+#      				 serial = line
+	   		if "Product:" in line:
+				 product = re.findall(r'Product:.*',line)
+#       				 product = line
+	    		if "Manufacturer:" in line:
+				 manufactur = re.findall(r'Manufacturer:.*',line)
+#       				 manufactur =  line
+
+		else:
+			usb_found = False
+
+
+if os == 'windows':
 	print "Windos"  
 
 
-print socket.getfqdn(socket.getfqdn())
-print getpass.getuser()
-print os
+if usb_found == 'True':
+	print os
+	print socket.getfqdn(socket.getfqdn())
+	print getpass.getuser()
+	username = getpass.getuser()
+	print serial
+	print product
+	print manufactur
+	db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="adduser", # your username
+                      passwd="Dhct3XQeBsDqKwcm", # your password
+                      db="usbinv") # name of the data base
+	cursor = db.cursor()
+
+	
+	serial = "e"
+	product = "d"
+	manufactur = "a"
+
+	sql = ("INSERT INTO usbinv (user, vendorname, type, serialnumber, os) VALUES(%s, %s, %s, %s, %s)",(username,manufactur,product,serial,os))	
+	print sql
+	cursor.execute(sql)
+	db.commit()
+	db.close()
+
+
