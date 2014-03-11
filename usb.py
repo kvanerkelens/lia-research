d5d759835dda99dc312830a6a69404a2361e5fb0
root <root@desktop-31.students.os3.nl>
2014-03-11 14:06:19 +0100

    Cleanup and added comments

--- usb.py
+++ usb.py
@@ -11,69 +11,52 @@ import time
 operatingsystem = platform.system()
 usb_found = "False"
 tmpfile = "/tmp/usbrunning"
-#logfile = "/tmp/usbinv.log"
-
-
-
-
-#file = open(logfile, "w")
-#file.write("usbinventory is running\n")
 
 
 if operatingsystem == 'Linux':
-	file.write("operating system = linux\n")
-
-	if os.path.isfile(tmpfile):
+	if os.path.isfile(tmpfile):								#script already running?
 		print "already running"
-		sys.exit(0)
-	else:
+		sys.exit(0)	
+	else:											#script not running create a file to show it is running									
 		file = open("tmpfile", "w")
 		file.write("usbinventory is running\n")
 		file.close()
 
 
-	dmesg = subprocess.Popen(["/tmp/usb.sh"], stdout = subprocess.PIPE).communicate()[0]
+	dmesg = subprocess.Popen(["/tmp/usb.sh"], stdout = subprocess.PIPE).communicate()[0]	#Compair dmesg and dmesg last time it ran.
 	for line in dmesg.splitlines():
 
-		if "> [" in line:
+		if "> [" in line: 								#get whats added to dmesg 
 
-    			if "SerialNumber:" in line:
-				serial = re.findall(r'(?<=SerialNumber: ).*',line)
+    			if "SerialNumber:" in line:						#If line contains serialnumber
+				serial = re.findall(r'(?<=SerialNumber: ).*',line)		#Grab the serial number
 				usb_found = "True"
 
-	   		if "Product:" in line:
-				product = re.findall(r'(?<=Product: ).*',line)
-	    		if "Manufacturer:" in line:
-				manufactur = re.findall(r'(?<=Manufacturer: ).*',line)
+	   		if "Product:" in line:							#if line contains product
+				product = re.findall(r'(?<=Product: ).*',line)			#grab product name
+	    		if "Manufacturer:" in line:						
+				manufactur = re.findall(r'(?<=Manufacturer: ).*',line)		#grab manufacturer
 		else:
 			usb_found = False
 
 	time.sleep(1)
-if operatingsystem == 'windows':
+if operatingsystem == 'windows':								 # if windows then....
 	print "Windos"  
-#	file.write("Windows found\n")
 
 
 
 if usb_found == 'True':
-# 	file.write("USB found\n")
-	print operatingsystem
-	computer = socket.getfqdn(socket.getfqdn())
-	print socket.getfqdn(socket.getfqdn())
-#	print os.getlogin()
-#	username = os.getlogin()
-
-#	print getpass.getuser()
-#	username = getpass.getuser()
-        username = subprocess.Popen(["/tmp/who.sh"], stdout = subprocess.PIPE).communicate()[0]
-
-	print serial[0]
-	print product[0]
-	print manufactur[0]
-	db = MySQLdb.connect(host="145.100.104.168", # your host, usually localhost
-                     user="workstation", # your username
-                      passwd="AxjHFqWSdzuYVSaJ", # your password
-                      db="usbinv") # name of the data base
+	computer = socket.getfqdn(socket.getfqdn()) 						#get pcname
+        username = subprocess.Popen(["/tmp/who.sh"], stdout = subprocess.PIPE).communicate()[0] #get username out of who.sh
+#	print operatingsystem
+#	print computer
+#	print serial[0]
+#	print product[0]
+#	print manufactur[0]
+	db = MySQLdb.connect(host="145.100.104.168", 
+                     user="workstation", 
+                      passwd="AxjHFqWSdzuYVSaJ", 
+                      db="usbinv") 
 	cursor = db.cursor()
 
 	
@@ -83,7 +66,7 @@ if usb_found == 'True':
 #	db.commit()
 #	db.close()
 	try:
-		os.remove(tmpfile)
+		os.remove(tmpfile)								#remove the file that shows the script is running
 	except:
 		pass
 
