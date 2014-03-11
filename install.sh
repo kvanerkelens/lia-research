39a729eeb2a83864992682c3e65dd0e0fd512550
root <root@desktop-31.students.os3.nl>
2014-03-11 14:20:32 +0100

    fixed bugs

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-apt-get install python python-mysqldb
+apt-get install python python-mysqldb wget
 mkdir /etc/usbinv
 
+wget -P /etc/usbinv/ http://145.100.104.168/usbdevices/lia-research/99-usb.rules
 wget -P /etc/usbinv/ http://145.100.104.168/usbdevices/lia-research/usb.py
 wget -P /etc/usbinv/ http://145.100.104.168/usbdevices/lia-research/usb.sh
 wget -P /etc/usbinv/ http://145.100.104.168/usbdevices/lia-research/who.sh
-chmod /etc/usbinv/usb.py
-chmod /etc/usbinv/usb.sh
-chmod /etc/usbinv/who.sh
+chmod +x /etc/usbinv/usb.py
+chmod +x /etc/usbinv/usb.sh
+chmod +x /etc/usbinv/who.sh
 
 
-cp 99-usb.rules /etc/udev/rules.d/99-usb.rules
+cp /etc/usbinv/99-usb.rules /etc/udev/rules.d/99-usb.rules
 udevadm control --reload-rules
+
